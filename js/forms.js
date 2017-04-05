ec50971646422e31480c1090900d22110326ceee
shangke <shangke01@gmail.com>
2017-04-05 17:16:49 +0800

    重构表单验证模块

--- js/forms.js
+++ js/forms.js
@@ -1,33 +1,27 @@
-function Account(obj) {
-    this.username = obj.username;
-    this.account = obj.account;
-    this.pwdmd5 = obj.pwdmd5;
-    this.toString = () => JSON.stringify(this);
-}
+/***
+ * 设计原则：本模块不直接操作 DOM ，只提供数据处理的 API
+ ***/
 
-// 新近账号对象
-function createAccount() {
-    if (1 == arguments.length) {
-        return new Account(arguments[0]);
-    } else if (3 == arguments.length) {
-        var obj = {
-            username: arguments[0],
-            account: arguments[1],
-            pwdmd5: toMD5(arguments[2])
+var FORMS = {};
+
+// 验证账号是否存在，不存在则新建成功，账号信息存入 sessionStorage
+FORMS.signup = function(obj) {
+    // 用户账号类
+    class Account {
+        constructor(obj) {
+            this.username = obj.username;
+            this.account = obj.account;
+            this.pwdmd5 = obj.pwdmd5;
         }
-        return new Account(obj);
-    } else {
-        return null;
+        toString() {
+            return JSON.stringify(this);
+        }
     }
-}
 
-
-// 验证账号是否存在，不存在则新建成功，账号信息存入 localStorage
-export function signup(username, account, pwd) {
-    var accountObj = localStorage.getItem(account);
+    var accountObj = sessionStorage.getItem(obj.account);
     if (null === accountObj) {
-        accountObj = createAccount(username, account, pwd);
-        localStorage.setItem(accountObj.account, accountObj.toString());
+        accountObj = new Account(obj);
+        sessionStorage.setItem(accountObj.account, accountObj.toString());
         return true;
     } else {
         return false;
@@ -36,71 +30,89 @@ export function signup(username, account, pwd) {
 
 
 // 验证登陆是否成功
-export function signin(account, pwd) {
-    var accountObj = JSON.parse(localStorage.getItem(account));
-    if (!accountObj) {
+FORMS.signin = function(account, pwd) {
+    // 从 Storage 中取出数据并验证存在性
+    var accountObj = JSON.parse(sessionStorage.getItem(account));
+    if (!accountObj) { // 如果账号不存在
         return false;
-    } else if (toMD5(pwd) === accountObj.pwdmd5) {
+    } else if (toMD5(pwd) === accountObj.pwdmd5) { // 密码正确
         return true;
-    } else {
+    } else { // 密码不正确
         return false;
     }
 }
 
 
-// 验证表单输入是否合法
-export function inputTest(username, account, pwd) {
-    // 错误信息
-    // 返回 0 表示一切正常
-    // 返回 1 表示字段为空
-    // 返回 2 表示格式错误
-    var errInfo = '';
+FORMS.inputTest = function(user, acc, pw) {
+    /***
+     * 验证表单输入是否合法
+     * 参数列表：用户输入，包括 username, account, password
+     * 返回值：返回一个 Promise 对象
+     **/
+    var username = user;
+    var account = acc;
+    var pwd = pw;
 
-    // 用于账号合法性验证
-    var telReg = /\d{11}/;
-    var emailReg = /[\w\.\_\-]+@\w+\.[a-zA-Z]+?[\.a-zA-Z]*?[a-zA-Z]+$/;
+    return new Promise(function(resolve, reject) {
+        // 错误信息
+        // 返回 0 表示一切正常
+        // 返回 1 表示字段为空
+        // 返回 2 表示格式错误
+        var errInfo = '';
 
-    String.prototype.getLength = function() {
-        var length = {
-            len: 0,
-            characters: 0,
-            letters: 0
-        };
+        // 用于账号合法性验证
+        var telReg = /^1[3|4|5|7|8][0-9]{9}$/;
 
-        for (var i = 0; i < this.length; i++) {
-            if (this.charCodeAt(i) > 127 || this.charCodeAt(i) == 94) {
-                length.len += 2;
-                length.characters++;
-            } else {
-                len++;
-                length.letters++;
+        String.prototype.getLength = function() {
+            var length = {
+                len: 0,
+                characters: 0,
+                letters: 0
+            };
+
+            for (var i = 0; i < this.length; i++) {
+                if (this.charCodeAt(i) > 127 || this.charCodeAt(i) == 94) {
+                    length.len += 2;
+                    length.characters++;
+                } else {
+                    length.len++;
+                    length.letters++;
+                }
             }
+            return length;
         }
-        return length;
-    }
 
-    var namelen = username.getLength();
-    if (0 == namelen.len) {
-        errInfo += '1';
-    } else if (1 == namelen.len || (2 == namelen.len && 0 == namelen.characters)){
-        errInfo += '2';
-    } else {
-        errInfo += '0';
-    }
-    if (!account) {
-        errInfo += '1';
-    } else if (!(telReg.test(account) || emailReg.test(account))) {
-        errInfo += '2';
-    } else {
-        errInfo += '0';
-    }
-    if (!pwd) {
-        errInfo += '1';
-    } else if (pwd.length < 6 || pwd.length > 128) {
-        errInfo += '2';
-    } else {
-        errInfo += '0';
-    }
+        var namelen = username.getLength();
+        if (0 == namelen.len) {
+            errInfo += '1';
+        } else if (1 == namelen.len || (2 == namelen.len && 0 == namelen.characters)) {
+            errInfo += '2';
+        } else {
+            errInfo += '0';
+        }
+        if (!account) {
+            errInfo += '1';
+        } else if (!telReg.test(account)) {
+            errInfo += '2';
+        } else {
+            errInfo += '0';
+        }
+        if (!pwd) {
+            errInfo += '1';
+        } else if (pwd.length < 6 || pwd.length > 128) {
+            errInfo += '2';
+        } else {
+            errInfo += '0';
+        }
 
-    return errInfo;
+        if ('000' === errInfo) {
+            resolve({
+                username: username,
+                account: account,
+                pwdmd5: md5(pwd)
+            });
+        } else {
+            reject(errInfo);
+        }
+    });
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,92 @@
-import {signup, signin, inputTest} from forms;
+'use strict'
 
-$(function () {
+$(function() {
     $('.signin-wrapper').hide();
     $('#signup-option').addClass('nav-active');
     $('#signin-option').addClass('nav-deactive');
 });
+
+$('#signup-option').click(function() {
+    $('#signup-option').removeClass('nav-deactive').addClass('nav-active');
+    $('#signin-option').removeClass('nav-active').addClass('nav-deactive');
+    $('.signin-wrapper').hide();
+    $('.signup-wrapper').show();
+})
+
+$('#signin-option').click(function() {
+    $('#signin-option').removeClass('nav-deactive').addClass('nav-active');
+    $('#signup-option').removeClass('nav-active').addClass('nav-deactive');
+    $('.signup-wrapper').hide();
+    $('.signin-wrapper').show();
+})
+
+var $username = $('#signup [name=username]');
+var $account = $('#signup [name=account]');
+var $pwd = $('#signup [name=password]');
+
+// 点击输入框时将输入框后面可能存在的错误信息提示去除
+$username.click(function() {
+    $username.next('label').remove();
+})
+$account.click(function() {
+    $account.next('label').remove();
+})
+$pwd.click(function() {
+    $pwd.next('label').remove();
+})
+
+
+// “注册知乎” button
+$('#signup-btn').click(function() {
+    /*
+     * 首先验证输入是否合法
+     * 若合法则注册成功，调用 FORMS.signup() 将注册信息存储
+     */
+
+    // 输入验证，返回验证错误码
+    FORMS.inputTest($username.val(), $account.val(), $pwd.val())
+        .then(obj => {
+            var signupRes = FORMS.signup(obj);
+            if (!signupRes) {
+                $account.after('<label>账号已存在</label>');
+            } else {
+                $username.val('');
+                $account.val('');
+                $pwd.val('');
+                alert('注册成功');
+            }
+        })
+        .catch(e => {
+            // 姓名验证反馈
+            if ('1' === e[0]) {
+                $username.next('label').remove();
+                $username.after('<label>请填写姓名</label>');
+            } else if ('2' === e[0]) {
+                $username.next('label').remove();
+                $username.after('<label>姓名最短为2个汉字或3个英文字符</label>');
+            }
+
+            // 账号验证反馈
+            if ('1' === e[1]) {
+                $account.next('label').remove();
+                $account.after('<label>请填写手机号</label>');
+            } else if ('2' === e[1]) {
+                $account.next('label').remove();
+                $account.after('<label>请输入正确的手机号</label>');
+            }
+
+            // 密码验证反馈
+            if ('1' === e[2]) {
+                $pwd.next('label').remove();
+                $pwd.after('<label>请填写密码</label>');
+            } else if ('2' === e[2]) {
+                $pwd.next('label').remove();
+                $pwd.after('<label>请输入 6-128 位的密码');
+            }
+        })
+
+})
+
+$('signin-btn').click(function() {
+    var signinRes = FORMS.signin()
+})
